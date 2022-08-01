7bb9470352703890d85dabfca130ee2a6c4821d9
nataliemezher <89838078+nataliemezher@users.noreply.github.com>
2022-08-01 14:41:00 +0200

    start-game js sim

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+let playerScore = 0;
+let computerScore = 0;
 
+let playButton = document.querySelector(".intro-game button");
+let introShow = document.querySelector(".intro-game");
+let match = document.querySelector(".game-match");
+
+playButton.addEventListener("click", fadeIntro);
+
+function fadeIntro() {
+  introShow.classList.add("fadeout");
+  match.classList.add("fadein");
+}
