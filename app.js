12eeae2cdab4bf6103afed34e71afae1471a1235
nataliemezher <89838078+nataliemezher@users.noreply.github.com>
2022-08-01 17:18:31 +0200

    options and outcomes of it in ifs

--- app.js
+++ app.js
@@ -11,3 +11,61 @@ function fadeIntro() {
   introShow.classList.add("fadeout");
   match.classList.add("fadein");
 }
+function playGame() {
+  let options = document.querySelectorAll(".options button");
+  let playerHand = document.querySelector(".playerhand");
+  let computerHand = document.querySelector(".computerhand");
+
+  //make computerhand choose random
+  let computerOption = ["rock", "paper", "scissors"];
+
+  options.forEach((op) => {
+    op.addEventListener("click", function () {
+      let computerNum = Math.floor(Math.random() * 3);
+      let computerChoice = computerOption[computerNum];
+      //console.log(computerChoice);
+    });
+  });
+
+  //console.log(computerNum);
+}
+
+function comparingHands(playerChoice, computerChoice) {
+  let winner = document.querySelector(".winner");
+
+  //checking if tie
+  if (playerChoice === computerChoice) {
+    winner.textContent = "It's a tie!";
+    return;
+    //checking if ROCK is chosen
+  } else if (playerChoice === "rock") {
+    if (computerChoice === "scissors") {
+      winner.textContent = "Player wins!";
+      return;
+    } else {
+      winner.textContent = "Computer wins!";
+      return;
+    }
+    //checking if PAPER is chosen
+  } else if (playerChoice === "paper") {
+    if (computerChoice === "scissors") {
+      winner.textContent = "Computer Wins!";
+      return;
+    } else {
+      winner.textContent = "Player Wins!";
+      return;
+    }
+  } else if (playerChoice === "scissors") {
+    if (computerChoice === "rock") {
+      winner.textContent = "Computer Wins!";
+      return;
+    } else {
+      winner.textContent = "Player Wins!";
+      return;
+    }
+  } else {
+    return "Something went wrong";
+  }
+}
+
+playGame();
